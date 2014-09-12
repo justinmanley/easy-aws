dada6a8c4a9a583e7c9ab8b735cbf15527b94c9c
Justin Manley <manleyjster@gmail.com>
2014-09-12 11:50:56 -0500

    Configure git and install bower globally.

--- setup/assemble.sh
+++ setup/assemble.sh
@@ -2,17 +2,23 @@
 # Includes node.js setup
 
 sudo apt-get update
-sudo apt-get install git
-sudo apt-get install unzip
 
-# install java
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:webupd8team/java
+# install and configure git
+sudo apt-get install git -y
+git config --global user.email "manleyjster@gmail.com"
+git config --global user.name "Justin Manley"
+
+# unzip is required for data pipeline to run
+sudo apt-get install unzip -y
+
+# java is also required for data pipeline to run
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
-sudo apt-get install oracle-java7-installer
+sudo apt-get install oracle-java7-installer -y
 
 # install and set up nvm and nodejs
-sudo apt-get install build-essential libssl-dev
+sudo apt-get install build-essential libssl-dev -y
 curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh
 
 # need to close and reopen terminal to start using nvm
@@ -22,3 +28,4 @@ nvm alias default 0.11.13
 
 # install global dependencies
 npm install -g grunt-cli
+npm install -g bower
